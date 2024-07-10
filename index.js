63369574ee1f404b1505f2b270c4432fbcd8574d
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-07-10 23:16:37 +0530

    uploads folder creater function

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ import dotenv from "dotenv";
 import connectDB from "./src/config/databaseConfig.js";
 import { app } from "./src/app.js";
 import { connectCloudinary } from "./src/config/cloudinaryConfig.js";
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+    console.log("upload folder created");
+  fs.mkdirSync(uploadsDir);
+}
+
 dotenv.config()
+
 connectCloudinary();
+
+
 connectDB()
 .then(()=>{
     app.on("error",(error)=>{
