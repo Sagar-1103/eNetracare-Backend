f9b41f2fec32ea3ee8cdc89ef512a5d2dcf62297
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-07-10 16:30:16 +0530

    reverted back

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,10 +2,6 @@ import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
-import dotenv from "dotenv";
-dotenv.config()
-
-const adminEmails = [process.env.ADMIN_1];
 
 const verifyJWT = asyncHandler(async(req,_,next)=>{
 
@@ -27,16 +23,5 @@ const verifyJWT = asyncHandler(async(req,_,next)=>{
     }
 })
 
-const verifyAdmin = asyncHandler(async(req,_,next)=>{
-    try {
-        const userEmail = req.user?.email;
-        if (!adminEmails.includes(userEmail)) {
-            throw new ApiError(401,"You are not the admin.");
-        }
-        next();
-    } catch (error) {
-        throw new ApiError(401,error?.message || "Invalid Access Token")
-    }
-})
 
-export {verifyJWT,verifyAdmin}
+export {verifyJWT}
--- src/routes/testimonial.routes.js
+++ src/routes/testimonial.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import {upload} from "../middlewares/multer.middleware.js"
-import { verifyJWT ,verifyAdmin} from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { deleteTestimonial, getAllTestimonials, postTestimonial, updatePatientImage, updateTestimonialContent } from "../controllers/testimonial.controller.js";
 
 const router = Router();
 
 router.route("/").get(getAllTestimonials)
-router.route("/post").post(verifyJWT,verifyAdmin,upload.single("image"),postTestimonial)
-router.route("/update-image/:id").patch(verifyJWT,verifyAdmin,upload.single("image"),updatePatientImage)
-router.route("/update-content/:id").patch(verifyJWT,verifyAdmin,updateTestimonialContent)
-router.route("/delete/:id").delete(verifyJWT,verifyAdmin,deleteTestimonial)
+router.route("/post").post(verifyJWT,upload.single("image"),postTestimonial)
+router.route("/update-image/:id").patch(verifyJWT,upload.single("image"),updatePatientImage)
+router.route("/update-content/:id").patch(verifyJWT,updateTestimonialContent)
+router.route("/delete/:id").delete(verifyJWT,deleteTestimonial)
 
 export default router;
