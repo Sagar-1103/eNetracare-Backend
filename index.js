757d2f60d460587308e6698b3e6e7debb632e9b7
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-07-10 17:06:09 +0530

    minor changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import connectDB from "./src/config/databaseConfig.js";
 import { app } from "./src/app.js";
 import { connectCloudinary } from "./src/config/cloudinaryConfig.js";
 dotenv.config()
-
+connectCloudinary();
 connectDB()
 .then(()=>{
     app.on("error",(error)=>{
@@ -11,7 +11,6 @@ connectDB()
         throw error
     })
     app.listen(process.env.PORT||3000,()=>{
-        connectCloudinary();
         console.log(`Server running on port ${process.env.PORT}`);
     })
 })
