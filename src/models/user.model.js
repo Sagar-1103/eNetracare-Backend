cc80ce152a086e4cfd74e7cd151c49523f148398
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-07-16 19:40:59 +0530

    roles added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,9 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config();
+const adminEmails = [process.env.ADMIN_1];
 
 const userSchema = new Schema(
   {
@@ -10,7 +13,7 @@ const userSchema = new Schema(
       unique: true,
       lowercase: true,
       trim: true,
-      index:true,
+      index: true,
     },
     password: {
       type: String,
@@ -19,45 +22,56 @@ const userSchema = new Schema(
     refreshToken: {
       type: String,
     },
+    role: {
+      type: String,
+    },
   },
   { timestamps: true }
 );
 
-userSchema.pre("save",async function (next){
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password,10);
-    next();
-})
+  this.password = await bcrypt.hash(this.password, 10);
+  this.role = decideRole(this.email);
+  next();
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password,this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = function(){
-    return jwt.sign(
-        {
-            _id:this._id,
-            email:this.email,
-            username:this.username,
-            fullName:this.fullName
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-        }
-    )
-}
-userSchema.methods.generateRefreshToken = function(){
-    return jwt.sign(
-        {
-            _id:this._id
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+const decideRole = (userEmail) => {
+  if (!adminEmails.includes(userEmail)) {
+    return "user";
+  }
+  return "admin";
+};
 
 export const User = mongoose.model("User", userSchema);
