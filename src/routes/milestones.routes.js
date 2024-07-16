393185f41056729bb1d97a023070b9a4e68cd259
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-07-17 02:00:00 +0530

    admin middleware removed from routes

--- src/routes/milestones.routes.js
+++ src/routes/milestones.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { verifyJWT,verifyAdmin } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { deleteMilestone, getAllMilestones, postMilestone, updateMilestoneContent } from "../controllers/milestone.controller.js";
 
 const router  = Router();
 
 router.route("/").get(getAllMilestones)
-router.route("/post").post(verifyJWT,verifyAdmin,postMilestone)
-router.route("/update-content/:id").patch(verifyJWT,verifyAdmin,updateMilestoneContent)
-router.route("/delete/:id").delete(verifyJWT,verifyAdmin,deleteMilestone)
+router.route("/post").post(verifyJWT,postMilestone)
+router.route("/update-content/:id").patch(verifyJWT,updateMilestoneContent)
+router.route("/delete/:id").delete(verifyJWT,deleteMilestone)
 
 export default router;
